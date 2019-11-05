bbbd1d9cce12ab6adb25e6195380dc715670fa4c
Jeremiah Boby <mail@jeremiahboby.me>
2019-11-05 02:41:39 +0000

    Version 0.2.0
    Fixed issues with TeX rendering
    Make EventSources solution faster

--- nbwatch/__init__.py
+++ nbwatch/__init__.py
@@ -7,7 +7,7 @@ Options:
   -h --help  Show this screen.
 """
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 import time
 import base64
@@ -16,12 +16,12 @@ import nbformat
 
 from pathlib import Path
 from typing import Iterator
+from collections import defaultdict
 
 from docopt import docopt
 from nbconvert import HTMLExporter
 from flask import (
-    Flask, Response, request, session,
-    render_template, send_from_directory
+    Flask, Response, request, render_template
 )
 
 
@@ -34,6 +34,7 @@ arguments = docopt(
     __doc__,
     version=f'nbwatch {__version__}'
 )
+exports = defaultdict(str)
 source = Path(arguments.get('<file>'))
 
 
@@ -61,10 +62,11 @@ def watch(path: Path) -> Iterator[str]:
             contents = read_final(path)
             notebook = nbformat.reads(contents, as_version=4)
             body, resources = exporter.from_notebook_node(notebook)
-            session['body'] = body
-            session['inlining'] = resources.get('inlining')
-            body = base64.b64encode(body.encode()).decode()
-            yield f'data: {body}\n\n'
+            exports['body'] = body
+            exports['inlining'] = '\n'.join(
+                resources['inlining']['css']
+            )
+            yield f'data: \n\n'
             last_modified = path.stat().st_mtime
 
 
@@ -74,8 +76,8 @@ def index():
         return Response(watch(source), content_type='text/event-stream')
     return render_template(
         'index.html',
-        body=session.get('body'),
-        inlining=session.get('inlining')
+        body=exports['body'],
+        inlining=exports['inlining']
     )
 
 
